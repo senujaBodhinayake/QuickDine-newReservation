b2a4b332d2cdf7a51c055f9680ecf8cfdc568695
senujaBodhinayake <senujabodhinayake123@gmail.com>
2025-08-30 13:51:08 +0530

    initial Spring Boot scaffold

---
+++ src/main/java/com/example/QuickDineReservation/QuickDineReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickDineReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickDineReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickDineReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuickDineReservation/QuickDineReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickDineReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickDineReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
